6d75549f465cbc399ed1c85ab47f280275949f87
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2024-10-26 17:52:54 +0500

    added datatype practice

---
+++ data-types/program2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char alphabet1;
+    char alphabet2;
+    char alphabet3;
+    char alphabet4;
+    char alphabet5;
+    
+    cout << "Enter first alphabet: ";
+    cin >> alphabet1;
+    cout << "Enter second alphabet: ";
+    cin >> alphabet2;
+    cout << "Enter third alphabet: ";
+    cin >> alphabet3;
+    cout << "Enter fourth alphabet: ";
+    cin >> alphabet4;
+    cout << "Enter fifth alphabet: ";
+    cin >> alphabet5;
+    
+    cout << "You entered: " << alphabet1 << alphabet2 << alphabet3 << alphabet4 << alphabet5;
+    
+    return 0;
+}
